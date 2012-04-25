3e3d47825a6525fb419e07bcb5645143ff77634c
paul.augros@gmail.com <paul.augros@gmail.com@f3040f24-40b9-0ec0-7058-8868dd037f95>
2012-04-25 19:49:23 +0000

    Test on borrow for db conn.

--- src/com/areahomeschoolers/baconbits/server/spring/DataSourceFactory.java
+++ src/com/areahomeschoolers/baconbits/server/spring/DataSourceFactory.java
@@ -8,6 +8,7 @@ public class DataSourceFactory {
 	public DataSource createDataSource() {
 		BasicDataSource ds = new BasicDataSource();
 		ds.setUrl("jdbc:google:rdbms://baconbits-sql:areahomeschoolers/baconbits");
+		ds.setTestOnBorrow(true);
 
 		return ds;
 	}
