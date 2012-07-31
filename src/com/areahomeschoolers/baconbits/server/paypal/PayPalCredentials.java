fa4fc209e4d5e57b94b845137eedb066ef92dbdf
paul.augros@gmail.com <paul.augros@gmail.com@f3040f24-40b9-0ec0-7058-8868dd037f95>
2012-07-31 12:06:42 +0000

    Updated PayPal.

--- src/com/areahomeschoolers/baconbits/server/paypal/PayPalCredentials.java
+++ src/com/areahomeschoolers/baconbits/server/paypal/PayPalCredentials.java
@@ -15,14 +15,15 @@ public class PayPalCredentials {
 			credentials.setAPIUsername("weare.home.educators_api1.gmail.com");
 			credentials.setAPIPassword("BZFADA4CXJSCHYW8");
 			credentials.setSignature("AFcWxV21C7fd0v3bYYYRCpSSRl31AIiJDk-qOGV.J6VfFcbztTiAOVoS");
-			credentials.setAccountEmail("weare.home.educators_api1.gmail.com");
+			credentials.setAccountEmail("weare.home.educators@gmail.com");
+			credentials.setAppId("b9374d996b19e1a5a34abcc3f070b04f");
 		} else {
 			credentials.setAPIUsername("paul.a_1343673136_biz_api1.gmail.com");
 			credentials.setAPIPassword("1343673159");
 			credentials.setSignature("AirQtU1053N9JvGp9xtKZtxvbZojAv39XGRp8tQ8KFG.CaL5sQWMC8F6");
 			credentials.setAccountEmail("paul.a_1343673136_biz@gmail.com");
+			credentials.setAppId("APP-80W284485P519543T");
 		}
-		credentials.setAppId("APP-80W284485P519543T");
 
 		SimplePay p = new SimplePay();
 
