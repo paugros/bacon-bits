9f7add673f4e6b9712423e0c7c312bdf0358ede3
paul.augros@gmail.com <paul.augros@gmail.com@f3040f24-40b9-0ec0-7058-8868dd037f95>
2012-08-04 14:57:14 +0000

    All event registrations page.

--- src/com/areahomeschoolers/baconbits/client/content/event/EventParticipantListPage.java
+++ src/com/areahomeschoolers/baconbits/client/content/event/EventParticipantListPage.java
@@ -10,9 +10,12 @@ import com.areahomeschoolers.baconbits.client.util.PageUrl;
 import com.areahomeschoolers.baconbits.client.util.Url;
 import com.areahomeschoolers.baconbits.client.util.WidgetFactory;
 import com.areahomeschoolers.baconbits.client.util.WidgetFactory.ContentWidth;
+import com.areahomeschoolers.baconbits.client.widgets.cellview.EntityCellTable.SortDirection;
 import com.areahomeschoolers.baconbits.shared.dto.Arg.EventArg;
 import com.areahomeschoolers.baconbits.shared.dto.ArgMap;
+import com.areahomeschoolers.baconbits.shared.dto.ArgMap.Status;
 import com.areahomeschoolers.baconbits.shared.dto.EventParticipant;
+import com.areahomeschoolers.baconbits.shared.dto.UserGroup.AccessLevel;
 
 import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.Label;
@@ -21,8 +24,15 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public final class EventParticipantListPage implements Page {
 	private SimplePanel payContainer = new SimplePanel();
+	private boolean showAll = Url.getBooleanParameter("all");
+	private String title;
 
 	public EventParticipantListPage(final VerticalPanel page) {
+		if ((showAll && !Application.hasRole(AccessLevel.GROUP_ADMINISTRATORS)) || !Application.isAuthenticated()) {
+			new ErrorPage(PageError.NOT_AUTHORIZED);
+			return;
+		}
+
 		page.add(payContainer);
 
 		String paymentAction = Url.getParameter("ps");
@@ -48,40 +58,51 @@ public final class EventParticipantListPage implements Page {
 			}
 		}
 
-		if (!Application.isAuthenticated()) {
-			new ErrorPage(PageError.NOT_AUTHORIZED);
-			return;
+		ArgMap<EventArg> args = new ArgMap<EventArg>(EventArg.INCLUDE_FIELDS);
+		if (showAll) {
+			args.setStatus(Status.ACTIVE);
+		} else {
+			args.put(EventArg.PARENT_ID_PLUS_SELF, Application.getCurrentUser().getId());
 		}
-
-		ArgMap<EventArg> args = new ArgMap<EventArg>(EventArg.PARENT_ID_PLUS_SELF, Application.getCurrentUser().getId());
-		final String title = "My Event Registrations";
 		final EventParticipantCellTable table = new EventParticipantCellTable(args);
-		table.setDisplayColumns(ParticipantColumn.EVENT, ParticipantColumn.PARTICIPANT_NAME, ParticipantColumn.ADDED_DATE, ParticipantColumn.PRICE,
-				ParticipantColumn.FIELDS, ParticipantColumn.STATUS);
+		if (showAll) {
+			table.setDisplayColumns(ParticipantColumn.ATTENDED, ParticipantColumn.EVENT, ParticipantColumn.REGISTRANT_NAME, ParticipantColumn.PARTICIPANT_NAME,
+					ParticipantColumn.ADDED_DATE, ParticipantColumn.AGE, ParticipantColumn.PRICE, ParticipantColumn.FIELDS, ParticipantColumn.STATUS,
+					ParticipantColumn.EDIT_STATUS);
+			table.setDefaultSortColumn(ParticipantColumn.ADDED_DATE, SortDirection.SORT_DESC);
+			title = "Recent Event Registrations";
+			page.add(WidgetFactory.newSection(table, ContentWidth.maxWidth1150px));
+		} else {
+			table.setDisplayColumns(ParticipantColumn.EVENT, ParticipantColumn.PARTICIPANT_NAME, ParticipantColumn.ADDED_DATE, ParticipantColumn.PRICE,
+					ParticipantColumn.FIELDS, ParticipantColumn.STATUS);
+			title = "My Event Registrations";
+			page.add(WidgetFactory.newSection(table, ContentWidth.maxWidth1000px));
+		}
 		table.setTitle(title);
 
 		table.getTitleBar().addSearchControl();
 		table.getTitleBar().addExcelControl();
-		page.add(WidgetFactory.newSection(table, ContentWidth.maxWidth1000px));
 
 		table.addDataReturnHandler(new DataReturnHandler() {
 			@Override
 			public void onDataReturn() {
 				Application.getLayout().setPage(title, page);
 
-				int payCount = 0;
-				for (EventParticipant p : table.getFullList()) {
-					if (p.getPrice() > 0 && p.getStatusId() == 1) {
-						payCount++;
+				if (!showAll) {
+					int payCount = 0;
+					for (EventParticipant p : table.getFullList()) {
+						if (p.getPrice() > 0 && p.getStatusId() == 1) {
+							payCount++;
+						}
 					}
-				}
 
-				if (payCount > 0) {
-					Hyperlink l = new Hyperlink("You have " + payCount + " unpaid items. Click here to pay.", PageUrl.eventPayment());
-					l.addStyleName("largeText heavyPadding");
-					payContainer.setWidget(l);
-				} else {
-					payContainer.clear();
+					if (payCount > 0) {
+						Hyperlink l = new Hyperlink("You have " + payCount + " unpaid items. Click here to pay.", PageUrl.eventPayment());
+						l.addStyleName("largeText heavyPadding");
+						payContainer.setWidget(l);
+					} else {
+						payContainer.clear();
+					}
 				}
 			}
 		});
