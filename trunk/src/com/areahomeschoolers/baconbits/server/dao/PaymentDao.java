d3f25f52ea6e11ae7933e0fba047093430e035bf
paul.augros@gmail.com <paul.augros@gmail.com@f3040f24-40b9-0ec0-7058-8868dd037f95>
2013-04-16 12:48:39 +0000

    Access bug fix.

--- trunk/src/com/areahomeschoolers/baconbits/server/dao/PaymentDao.java
+++ trunk/src/com/areahomeschoolers/baconbits/server/dao/PaymentDao.java
@@ -2,8 +2,6 @@ package com.areahomeschoolers.baconbits.server.dao;
 
 import java.util.ArrayList;
 
-import org.springframework.security.access.prepost.PreAuthorize;
-
 import com.areahomeschoolers.baconbits.shared.dto.Arg.PaymentArg;
 import com.areahomeschoolers.baconbits.shared.dto.ArgMap;
 import com.areahomeschoolers.baconbits.shared.dto.Payment;
@@ -13,6 +11,5 @@ public interface PaymentDao {
 
 	public ArrayList<Payment> list(ArgMap<PaymentArg> args);
 
-	@PreAuthorize("hasRole('GROUP_ADMINISTRATORS')")
 	public Payment save(Payment payment);
 }
