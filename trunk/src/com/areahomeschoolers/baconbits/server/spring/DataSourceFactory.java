ba03513730b610cf46f39ae37415d1381a3a60b0
paul.augros@gmail.com <paul.augros@gmail.com@f3040f24-40b9-0ec0-7058-8868dd037f95>
2013-10-04 20:20:31 +0000

    Staging version.

--- trunk/src/com/areahomeschoolers/baconbits/server/spring/DataSourceFactory.java
+++ trunk/src/com/areahomeschoolers/baconbits/server/spring/DataSourceFactory.java
@@ -4,16 +4,25 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
 
+import com.google.appengine.api.utils.SystemProperty;
+
 public class DataSourceFactory {
 	public DataSource createDataSource() {
 		BasicDataSource ds = new BasicDataSource();
-		ds.setUrl("jdbc:google:rdbms://baconbits-sql:areahomeschoolers/baconbits");
+
+		String db = "production".equals(SystemProperty.version.get()) ? "areahomeschoolers" : "development";
+		ds.setUrl("jdbc:google:rdbms://baconbits-sql:" + db + "/baconbits");
 		ds.setValidationQuery("select max(id) from articles");
 		ds.setTestWhileIdle(true);
-		// ds.setTimeBetweenEvictionRunsMillis(1000 * 60);
 		ds.setTestOnBorrow(true);
 		ds.setInitialSize(1);
 		ds.setMinIdle(1);
+		ds.setMaxIdle(5);
+		ds.setMaxActive(100);
+		ds.setMaxWait(3 * 1000);
+		ds.setLogAbandoned(true);
+		ds.setRemoveAbandoned(true);
+		ds.setRemoveAbandonedTimeout(60);
 
 		return ds;
 	}
