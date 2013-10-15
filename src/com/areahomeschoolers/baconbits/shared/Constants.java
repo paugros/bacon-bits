11a5f1a109345e6ec5b567884749d887f2ffded9
paul.augros@gmail.com <paul.augros@gmail.com@f3040f24-40b9-0ec0-7058-8868dd037f95>
2013-10-15 15:08:32 +0000

    Colors

--- src/com/areahomeschoolers/baconbits/shared/Constants.java
+++ src/com/areahomeschoolers/baconbits/shared/Constants.java
@@ -34,7 +34,7 @@ public abstract class Constants {
 	public static final int DEFAULT_LOGO_ID = 1241;
 	public static final int ACCOUNT_CREATION_INSTRUCTIONS_ID = 77;
 	public static final int BOOK_SELLERS_GROUP_ID = 16;
-	public static final String CG_DOMAIN = "myhomeschoolgroups.com";
+	public static final String CG_DOMAIN = "citrusgroups.com";
 	public static final String CG_URL = "http://www." + CG_DOMAIN;
 	public static final String TOS_URL = CG_URL + "/#page=Article&articleId=73";
 	public static final String PRIVACY_POLICY_URL = CG_URL + "/#page=Article&articleId=72";
