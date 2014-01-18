a115c9dc1b2256547908c73143c0c6e86b41bcd4
paul.augros@gmail.com <paul.augros@gmail.com@f3040f24-40b9-0ec0-7058-8868dd037f95>
2014-01-18 16:47:51 +0000

    Link styles.

--- src/com/areahomeschoolers/baconbits/client/widgets/TitleBar.java
+++ src/com/areahomeschoolers/baconbits/client/widgets/TitleBar.java
@@ -16,12 +16,14 @@ import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
@@ -132,6 +134,9 @@ public class TitleBar extends Composite {
 	 * @param link
 	 */
 	public void addLink(Widget link) {
+		if (link instanceof Anchor || link instanceof Hyperlink || link instanceof ClickLabel) {
+			link.addStyleName("titleBarLink");
+		}
 		linkPanel.add(link);
 	}
 
